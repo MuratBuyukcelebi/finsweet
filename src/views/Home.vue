2f0f970497ca50f0422fb710b6020d007abfc7ae
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2022-01-24 01:41:32 +0300

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="button__list">
+      <button type="button" class="btn btn-primary">View our work</button>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.button__list {
+  padding: 100px;
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
