74896d26df7840385585c780fd03250728eb5f63
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2022-01-25 18:54:19 +0300

    feat: Footer Update

--- src/components/Layout/Footer.vue
+++ src/components/Layout/Footer.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="container">
-    <div class="footer__logo">
-      
+  <div class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-5">
+          <a href="#" class="footer__logo"><img src="../../assets/layout/logo.svg" alt="logo"></a>
+          <div class="footer__description">We are always open to discuss your project and improve your online presence.</div>
+          <div class="footer__contact">
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -12,6 +19,25 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+@import "src/assets/scss/main";
 
+  .footer {
+    background-color: $brand-tint-blue;
+
+    &__logo {
+      margin-bottom: 22px;
+
+      img {
+        height: 38px;
+      }
+    }
+    &__description {
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 28px;
+      color: #FFFFFF;
+      opacity: 0.8;
+    }
+  }
 </style>
