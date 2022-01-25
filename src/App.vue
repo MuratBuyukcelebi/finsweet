7a1f8352eec82c1731c84b2b4e57bf240a9f5f73
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2022-01-25 14:58:35 +0300

    feat: create desktop header

--- src/App.vue
+++ src/App.vue
@@ -21,5 +21,9 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+
+  a {
+    text-decoration: none;
+  }
 }
 </style>
--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="container">
-
+  <div class="header">
+    <div class="container header__container">
+      <a href="#" class="header__logo"><img src="../../../src/assets/layout/logo.svg" alt="logo"></a>
+      <nav class="header__menu">
+        <a href="#">Home</a>
+        <a href="#">About us</a>
+        <a href="#">Features</a>
+        <a href="#">Pricing</a>
+        <a href="#">FAQ</a>
+        <a href="#">Blog</a>
+      </nav>
+      <a href="#" class="header__button">Contact us</a>
+    </div>
   </div>
 </template>
 
@@ -10,6 +21,58 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+  @import "src/assets/scss/main";
 
+  .header {
+    background-color: $brand-tint-blue;
+    padding: 16px 0;
+
+    &__container {
+      display: flex;
+      align-items: center;
+    }
+    &__logo {
+      margin-right: auto;
+
+      img {
+        height: 26px;
+      }
+    }
+    &__menu {
+      display: flex;
+      align-items: center;
+      gap: 32px;
+      margin-right: 48px;
+
+      a {
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 28px;
+        color: #BBBBCB;
+        transition: all .2s;
+
+        &:hover {
+          color: #fff;
+        }
+      }
+    }
+    &__button {
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 28px;
+      color: #FFFFFF !important;
+      padding: 16px 48px;
+      border: 2px solid rgba(244, 246, 252, 0.2);
+      border-radius: 40px;
+      transition: all .2s;
+
+      &:hover {
+        border-color: rgba(244, 246, 252, 0.4);
+      }
+      &:active {
+        transform: translateY(2px);
+      }
+    }
+  }
 </style>
