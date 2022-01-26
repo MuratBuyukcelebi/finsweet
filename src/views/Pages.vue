18e8716a379c2fe812c87e2523d3ea936fea978e
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2022-01-26 23:54:20 +0300

    Feat: create pages

---
+++ src/views/Pages.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-dark" style="height: 100vh;">
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+        <div class="col-md-8">
+          <div class="card text-white bg-secondary w-100 my-5">
+            <div class="card-header text-center">Sayfalar</div>
+            <div class="card-body">
+              <div class="d-grid gap-2">
+                <a target="_blank" href="/" class="btn btn-dark btn-block btn-lg">Anasayfa</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pages"
+}
+</script>
