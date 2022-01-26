8a30f33dadb289262d8fdf8a9151abd8a7169d5e
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2022-01-26 23:49:35 +0300

    feat: create layout

---
+++ src/components/Layout/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header/>
+    <router-view/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default {
+  components: {Footer, Header},
+  name: "App"
+}
+</script>
+
+<style scoped>
+</style>
