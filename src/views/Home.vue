ed4ed49479d08ed8b16426f79a58b51be96dbcca
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2022-02-03 22:35:03 +0300

    feat: create Home --> Features

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,13 @@
     <hero/>
     <work/>
     <project/>
+    <features/>
   </div>
 </template>
 
 <style lang="scss">
+@import "../assets/scss/main";
+
 .button__list {
   padding: 100px;
   display: flex;
@@ -17,8 +20,9 @@
 import Hero from "../components/Home/Hero";
 import Work from "../components/Home/Work";
 import Project from "../components/Home/Project";
+import Features from "../components/Home/Features";
 
 export default {
-  components: {Project, Work, Hero}
+  components: {Features, Project, Work, Hero}
 }
 </script>
