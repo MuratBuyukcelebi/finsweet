c998e57aaaf011be1ed60448f047727942997131
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2022-02-15 00:00:09 +0300

    feat: About

---
+++ src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="About">
+    <Hero/>
+    <We/>
+    <Process/>
+  </div>
+</template>
+
+<script>
+import Hero from "../components/About/Hero";
+import We from "../components/About/We";
+import Process from "../components/About/Process";
+export default {
+  name: "About",
+  components: {Process, Hero, We}
+}
+</script>
+
+<style scoped>
+
+</style>
