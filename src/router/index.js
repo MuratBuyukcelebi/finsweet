94b7f18aa559d447f5853b6c0ab8f743e8c27ccb
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2022-02-18 23:48:17 +0300

    feat: Pricing

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from "../views/Home"
 import About from "../views/About"
+import Pricing from "../views/Pricing"
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,10 @@ const routes = [
       {
         path: '/about',
         component: About,
+      },
+      {
+        path: '/pricing',
+        component: Pricing,
       }
     ]
   },
---
+++ src/views/Pricing.vue
@@ -0,0 +1,291 @@
+<template>
+  <div class="container">
+    <div class="pricing">
+      <div class="pricing__top">
+        <div class="pricing__top-title">Our Pricing Plans</div>
+        <div class="pricing__top-description">When you’re ready to go beyond prototyping in Figma, Webflow is ready to help you bring your designs to life — without coding them.</div>
+      </div>
+      <div class="row">
+        <div class="col-xl-4">
+          <div class="pricing-item">
+            <div class="pricing-item__top">
+              <span class="pricing-item__top-price">$299</span>
+              <span class="pricing-item__top-limit">Per Design</span>
+            </div>
+            <div class="pricing-item__location">
+              <div class="pricing-item__location-title">Landing Page </div>
+              <div class="pricing-item__location-description">When you’re ready to go beyond prototyping in Figma, </div>
+            </div>
+            <div class="pricing-item__feature">
+              <div class="pricing-item__feature-item pricing-item__feature-item--active">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">All limited links</div>
+              </div>
+              <div class="pricing-item__feature-item pricing-item__feature-item--active">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">Own analytics platform</div>
+              </div>
+              <div class="pricing-item__feature-item pricing-item__feature-item--active">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">Chat support</div>
+              </div>
+              <div class="pricing-item__feature-item">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">Optimize hashtags</div>
+              </div>
+              <div class="pricing-item__feature-item">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">Unlimited users</div>
+              </div>
+            </div>
+            <a href="#" class="btn btn-secondary pricing-item__button">Get Start</a>
+          </div>
+        </div>
+        <div class="col-xl-4">
+          <div class="pricing-item">
+            <div class="pricing-item__top">
+              <span class="pricing-item__top-price">$299</span>
+              <span class="pricing-item__top-limit">Per Design</span>
+            </div>
+            <div class="pricing-item__location">
+              <div class="pricing-item__location-title">Landing Page </div>
+              <div class="pricing-item__location-description">When you’re ready to go beyond prototyping in Figma, </div>
+            </div>
+            <div class="pricing-item__feature">
+              <div class="pricing-item__feature-item pricing-item__feature-item--active">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">All limited links</div>
+              </div>
+              <div class="pricing-item__feature-item pricing-item__feature-item--active">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">Own analytics platform</div>
+              </div>
+              <div class="pricing-item__feature-item pricing-item__feature-item--active">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">Chat support</div>
+              </div>
+              <div class="pricing-item__feature-item">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">Optimize hashtags</div>
+              </div>
+              <div class="pricing-item__feature-item">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">Unlimited users</div>
+              </div>
+            </div>
+            <a href="#" class="btn btn-secondary pricing-item__button">Get Start</a>
+          </div>
+        </div>
+        <div class="col-xl-4">
+          <div class="pricing-item">
+            <div class="pricing-item__top">
+              <span class="pricing-item__top-price">$299</span>
+              <span class="pricing-item__top-limit">Per Design</span>
+            </div>
+            <div class="pricing-item__location">
+              <div class="pricing-item__location-title">Landing Page </div>
+              <div class="pricing-item__location-description">When you’re ready to go beyond prototyping in Figma, </div>
+            </div>
+            <div class="pricing-item__feature">
+              <div class="pricing-item__feature-item pricing-item__feature-item--active">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">All limited links</div>
+              </div>
+              <div class="pricing-item__feature-item pricing-item__feature-item--active">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">Own analytics platform</div>
+              </div>
+              <div class="pricing-item__feature-item pricing-item__feature-item--active">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">Chat support</div>
+              </div>
+              <div class="pricing-item__feature-item">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">Optimize hashtags</div>
+              </div>
+              <div class="pricing-item__feature-item">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">Unlimited users</div>
+              </div>
+            </div>
+            <a href="#" class="btn btn-secondary pricing-item__button">Get Start</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pricing"
+}
+</script>
+
+<style lang="scss" scoped>
+.pricing {
+  padding-top: 128px;
+  padding-bottom: 128px;
+
+  &__top {
+    margin-bottom: 64px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+
+    &-title {
+      font-weight: 600;
+      font-size: 48px;
+      line-height: 64px;
+      color: $brand-dark-blue;
+      margin-bottom: 16px;
+    }
+    &-description {
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 28px;
+      color: $brand-dark-blue;
+      opacity: 0.7;
+      max-width: 562px;
+    }
+  }
+}
+.pricing-item {
+  padding: 64px 48px;
+  background: $brand-gray;
+  border-radius: 12px;
+
+  &__top {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    margin-bottom: 20px;
+
+    &-price {
+      font-weight: 600;
+      font-size: 38px;
+      line-height: 56px;
+      color: $brand-dark-blue;
+    }
+    &-limit {
+      font-size: 16px;
+      line-height: 28px;
+      color: $brand-royal-blue;
+    }
+  }
+  &__location {
+    margin-bottom: 50px;
+
+    &-title {
+      font-weight: 500;
+      font-size: 24px;
+      line-height: 36px;
+      color: $brand-dark-blue;
+      margin-bottom: 8px;
+    }
+    &-description {
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 28px;
+      opacity: .7;
+      color: $brand-dark-blue;
+    }
+  }
+  &__feature {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    margin-bottom: 80px;
+
+    &-item {
+      display: flex;
+      align-items: center;
+      gap: 12px;
+
+      &__text {
+        font-size: 16px;
+        line-height: 28px;
+        color: $brand-dark-blue;
+        opacity: 0.7;
+      }
+
+      &--active & {
+        &__icon {
+          svg {
+            path {
+              fill: #97DAAD;
+            }
+          }
+        }
+        &__text {
+          opacity: 1;
+        }
+      }
+    }
+  }
+  &__button {
+    width: 100%;
+  }
+}
+</style>
