39a64a9304067c968e62ab3ab92cd338c2362fe9
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2022-02-19 23:37:29 +0300

    chore: Pricing

--- src/views/Pricing.vue
+++ src/views/Pricing.vue
@@ -119,12 +119,12 @@
         <div class="col-xl-4">
           <div class="pricing-item">
             <div class="pricing-item__top">
-              <span class="pricing-item__top-price">$399</span>
+              <span class="pricing-item__top-price">$499 +</span>
               <span class="pricing-item__top-limit">Per Design</span>
             </div>
             <div class="pricing-item__location">
-              <div class="pricing-item__location-title">Landing Page </div>
-              <div class="pricing-item__location-description">When you’re ready to go beyond prototyping in Figma, </div>
+              <div class="pricing-item__location-title">Complex Project</div>
+              <div class="pricing-item__location-description">When you’re ready to go beyond prototyping in Figma,</div>
             </div>
             <div class="pricing-item__feature">
               <div class="pricing-item__feature-item pricing-item__feature-item--active">
@@ -166,6 +166,14 @@
                   </svg>
                 </div>
                 <div class="pricing-item__feature-item__text">Unlimited users</div>
+              </div>
+              <div class="pricing-item__feature-item pricing-item__feature-item--active">
+                <div class="pricing-item__feature-item__icon">
+                  <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+                  </svg>
+                </div>
+                <div class="pricing-item__feature-item__text">Assist and Help</div>
               </div>
             </div>
             <a href="#" class="btn btn-secondary pricing-item__button">Get Start</a>
