694e133ba85e78d285ce99c00757c2e1d4052b67
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2022-02-27 01:58:33 +0300

    feat: Work --> Project

---
+++ src/components/Work/Project.vue
@@ -0,0 +1,220 @@
+<template>
+  <div class="container">
+    <div class="project">
+      <ul class="project__menu">
+        <li class="active">All</li>
+        <li>UI Design</li>
+        <li>Webflow Design</li>
+        <li>Figma Design</li>
+      </ul>
+      <div class="row project__list">
+        <div class="col-xl-6">
+          <div class="project-item">
+            <div class="project-item__image" style="--background: linear-gradient(var(--bgRotate), #F4EDE1 16.74%, #98B1C6 103.17%);">
+              <img src="../../assets/work/project/1.png" alt="image">
+            </div>
+            <div class="project-item__title">Template 1</div>
+            <div class="project-item__description">Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.</div>
+            <a href="#" class="project-item__button">
+              <span>View Portfolio</span>
+              <svg width="25" height="12" viewBox="0 0 25 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M24.5303 6.53033C24.8232 6.23744 24.8232 5.76256 24.5303 5.46967L19.7574 0.696699C19.4645 0.403806 18.9896 0.403806 18.6967 0.696699C18.4038 0.989593 18.4038 1.46447 18.6967 1.75736L22.9393 6L18.6967 10.2426C18.4038 10.5355 18.4038 11.0104 18.6967 11.3033C18.9896 11.5962 19.4645 11.5962 19.7574 11.3033L24.5303 6.53033ZM0 6.75H24V5.25H0V6.75Z" fill="#F4F6FC"/>
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div class="col-xl-6">
+          <div class="project-item">
+            <div class="project-item__image" style="--background: linear-gradient(var(--bgRotate), #FFD3AF 16.74%, #4F56FF 103.17%);">
+              <img src="../../assets/work/project/2.png" alt="image">
+            </div>
+            <div class="project-item__title">Template 2</div>
+            <div class="project-item__description">Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.</div>
+            <a href="#" class="project-item__button">
+              <span>View Portfolio</span>
+              <svg width="25" height="12" viewBox="0 0 25 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M24.5303 6.53033C24.8232 6.23744 24.8232 5.76256 24.5303 5.46967L19.7574 0.696699C19.4645 0.403806 18.9896 0.403806 18.6967 0.696699C18.4038 0.989593 18.4038 1.46447 18.6967 1.75736L22.9393 6L18.6967 10.2426C18.4038 10.5355 18.4038 11.0104 18.6967 11.3033C18.9896 11.5962 19.4645 11.5962 19.7574 11.3033L24.5303 6.53033ZM0 6.75H24V5.25H0V6.75Z" fill="#F4F6FC"/>
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div class="col-xl-6">
+          <div class="project-item">
+            <div class="project-item__image" style="--background: linear-gradient(var(--bgRotate), #EEBCA7 16.74%, #C8C8C8 103.17%);">
+              <img src="../../assets/work/project/3.png" alt="image">
+            </div>
+            <div class="project-item__title">Template 3</div>
+            <div class="project-item__description">Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.</div>
+            <a href="#" class="project-item__button">
+              <span>View Portfolio</span>
+              <svg width="25" height="12" viewBox="0 0 25 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M24.5303 6.53033C24.8232 6.23744 24.8232 5.76256 24.5303 5.46967L19.7574 0.696699C19.4645 0.403806 18.9896 0.403806 18.6967 0.696699C18.4038 0.989593 18.4038 1.46447 18.6967 1.75736L22.9393 6L18.6967 10.2426C18.4038 10.5355 18.4038 11.0104 18.6967 11.3033C18.9896 11.5962 19.4645 11.5962 19.7574 11.3033L24.5303 6.53033ZM0 6.75H24V5.25H0V6.75Z" fill="#F4F6FC"/>
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div class="col-xl-6">
+          <div class="project-item">
+            <div class="project-item__image" style="--background: linear-gradient(var(--bgRotate), #C2E7F2 16.74%, #98A4FF 103.17%);">
+              <img src="../../assets/work/project/4.png" alt="image">
+            </div>
+            <div class="project-item__title">Template 4</div>
+            <div class="project-item__description">Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.</div>
+            <a href="#" class="project-item__button">
+              <span>View Portfolio</span>
+              <svg width="25" height="12" viewBox="0 0 25 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M24.5303 6.53033C24.8232 6.23744 24.8232 5.76256 24.5303 5.46967L19.7574 0.696699C19.4645 0.403806 18.9896 0.403806 18.6967 0.696699C18.4038 0.989593 18.4038 1.46447 18.6967 1.75736L22.9393 6L18.6967 10.2426C18.4038 10.5355 18.4038 11.0104 18.6967 11.3033C18.9896 11.5962 19.4645 11.5962 19.7574 11.3033L24.5303 6.53033ZM0 6.75H24V5.25H0V6.75Z" fill="#F4F6FC"/>
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div class="col-xl-6">
+          <div class="project-item">
+            <div class="project-item__image" style="--background: linear-gradient(var(--bgRotate),  #C5BCFF 16.74%, #9BF7D5 103.17%);">
+              <img src="../../assets/work/project/5.png" alt="image">
+            </div>
+            <div class="project-item__title">Template 5</div>
+            <div class="project-item__description">Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.</div>
+            <a href="#" class="project-item__button">
+              <span>View Portfolio</span>
+              <svg width="25" height="12" viewBox="0 0 25 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M24.5303 6.53033C24.8232 6.23744 24.8232 5.76256 24.5303 5.46967L19.7574 0.696699C19.4645 0.403806 18.9896 0.403806 18.6967 0.696699C18.4038 0.989593 18.4038 1.46447 18.6967 1.75736L22.9393 6L18.6967 10.2426C18.4038 10.5355 18.4038 11.0104 18.6967 11.3033C18.9896 11.5962 19.4645 11.5962 19.7574 11.3033L24.5303 6.53033ZM0 6.75H24V5.25H0V6.75Z" fill="#F4F6FC"/>
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div class="col-xl-6">
+          <div class="project-item">
+            <div class="project-item__image" style="--background: linear-gradient(var(--bgRotate), #D2D6FD 16.74%, #D6ACEA 103.17%);">
+              <img src="../../assets/work/project/6.png" alt="image">
+            </div>
+            <div class="project-item__title">Template 6</div>
+            <div class="project-item__description">Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.</div>
+            <a href="#" class="project-item__button">
+              <span>View Portfolio</span>
+              <svg width="25" height="12" viewBox="0 0 25 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M24.5303 6.53033C24.8232 6.23744 24.8232 5.76256 24.5303 5.46967L19.7574 0.696699C19.4645 0.403806 18.9896 0.403806 18.6967 0.696699C18.4038 0.989593 18.4038 1.46447 18.6967 1.75736L22.9393 6L18.6967 10.2426C18.4038 10.5355 18.4038 11.0104 18.6967 11.3033C18.9896 11.5962 19.4645 11.5962 19.7574 11.3033L24.5303 6.53033ZM0 6.75H24V5.25H0V6.75Z" fill="#F4F6FC"/>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Project"
+}
+</script>
+
+<style lang="scss" scoped>
+.project {
+  padding-top: 96px;
+  padding-bottom: 96px;
+
+  &__menu {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 32px;
+    margin-bottom: 48px;
+    list-style-type: none;
+
+    li {
+      font-weight: 500;
+      font-size: 18px;
+      line-height: 32px;
+      color: $brand-dark-blue;
+      opacity: 0.7;
+      cursor: pointer;
+
+      &.active {
+        color: $brand-royal-blue;
+        opacity: 1;
+      }
+    }
+  }
+  &__list {
+    .col-xl-6:nth-last-child(n+3) {
+      .project-item {
+        margin-bottom: 64px;
+      }
+    }
+  }
+}
+.project-item {
+  &__image {
+    --bgRotate: 130deg;
+    background: var(--background);
+    width: 100%;
+    height: 100%;
+    padding-top: 34px;
+    padding-bottom: 34px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 24px;
+    animation: rotate 8s linear infinite;
+
+    img {
+      height: 317px;
+      width: auto;
+      object-fit: cover;
+    }
+  }
+  &__title {
+    font-weight: 600;
+    font-size: 38px;
+    line-height: 56px;
+    margin-bottom: 12px;
+    color: $brand-dark-blue;
+  }
+  &__description {
+    font-size: 16px;
+    line-height: 28px;
+    text-align: justify;
+    color: #282938;
+    opacity: 0.7;
+    margin-bottom: 24px;
+  }
+  &__button {
+    display: flex;
+    gap: 16px;
+    align-items: center;
+
+    span {
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 150%;
+      color: $brand-dark-blue;
+    }
+    svg {
+      transition: all .3s;
+
+      path {
+        fill: $brand-dark-blue;
+      }
+    }
+
+    &:hover {
+      svg {
+        transform: translateX(6px);
+      }
+    }
+  }
+}
+
+@keyframes rotate {
+  to {
+    --bgRotate: 490deg;
+  }
+}
+
+@property --bgRotate {
+  syntax: '<angle>';
+  initial-value: 490deg;
+  inherits: false;
+}
+</style>
