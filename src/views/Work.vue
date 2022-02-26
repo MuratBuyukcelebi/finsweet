393ee83f68791b52d395c9ed97492442126547d6
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2022-02-27 00:07:55 +0300

    feat: Work --> Hero

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="work">
     <Hero/>
+    <Project/>
   </div>
 </template>
 
 <script>
 import Hero from "../components/Work/Hero";
+import Project from "../components/Work/Project";
 export default {
   name: "Work",
-  components: {Hero}
+  components: {Project, Hero}
 }
 </script>
 
