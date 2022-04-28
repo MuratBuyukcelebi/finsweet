dce1dd01fe4ad380d0fe225d4845d398f9dcb920
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2022-04-28 21:40:05 +0300

    chore: blogs passed to the component system

--- src/components/Home/Blog.vue
+++ src/components/Home/Blog.vue
@@ -2,40 +2,12 @@
   <div class="blog container">
     <div class="blog__title">Our blog</div>
     <div class="row gy-lg-0 gy-5">
-      <div class="col-lg-4">
+      <div class="col-lg-4" v-for="item in blog">
         <div class="blog-item">
-          <div class="blog-item__image"><img src="../../assets/home/blog/1.png" alt="image"></div>
-          <div class="blog-item__history">19 Jan 2022</div>
-          <div class="blog-item__title">How one Webflow user grew his single person consultancy from $0-100K in 14 months</div>
-          <div class="blog-item__description">See how pivoting to Webflow changed one person’s sales strategy and allowed him to attract</div>
-          <a href="#" class="blog-item__button">
-            <span>Read More</span>
-            <svg width="25" height="12" viewBox="0 0 25 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M24.5303 6.53033C24.8232 6.23744 24.8232 5.76256 24.5303 5.46967L19.7574 0.696699C19.4645 0.403806 18.9896 0.403806 18.6967 0.696699C18.4038 0.989593 18.4038 1.46447 18.6967 1.75736L22.9393 6L18.6967 10.2426C18.4038 10.5355 18.4038 11.0104 18.6967 11.3033C18.9896 11.5962 19.4645 11.5962 19.7574 11.3033L24.5303 6.53033ZM0 6.75H24V5.25H0V6.75Z" fill="#F4F6FC"/>
-            </svg>
-          </a>
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="blog-item">
-          <div class="blog-item__image"><img src="../../assets/home/blog/2.png" alt="image"></div>
-          <div class="blog-item__history">19 Jan 2022</div>
-          <div class="blog-item__title">How one Webflow user grew his single person consultancy from $0-100K in 14 months</div>
-          <div class="blog-item__description">See how pivoting to Webflow changed one person’s sales strategy and allowed him to attract</div>
-          <a href="#" class="blog-item__button">
-            <span>Read More</span>
-            <svg width="25" height="12" viewBox="0 0 25 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M24.5303 6.53033C24.8232 6.23744 24.8232 5.76256 24.5303 5.46967L19.7574 0.696699C19.4645 0.403806 18.9896 0.403806 18.6967 0.696699C18.4038 0.989593 18.4038 1.46447 18.6967 1.75736L22.9393 6L18.6967 10.2426C18.4038 10.5355 18.4038 11.0104 18.6967 11.3033C18.9896 11.5962 19.4645 11.5962 19.7574 11.3033L24.5303 6.53033ZM0 6.75H24V5.25H0V6.75Z" fill="#F4F6FC"/>
-            </svg>
-          </a>
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="blog-item">
-          <div class="blog-item__image"><img src="../../assets/home/blog/3.png" alt="image"></div>
-          <div class="blog-item__history">19 Jan 2022</div>
-          <div class="blog-item__title">How one Webflow user grew his single person consultancy from $0-100K in 14 months</div>
-          <div class="blog-item__description">See how pivoting to Webflow changed one person’s sales strategy and allowed him to attract</div>
+          <div class="blog-item__image"><img :src="require(`../../assets/home/blog/${item.image}.png`)" alt="image"></div>
+          <div class="blog-item__history">{{ item.date }}</div>
+          <div class="blog-item__title"> {{ item.title }} </div>
+          <div class="blog-item__description">{{ item.description }}</div>
           <a href="#" class="blog-item__button">
             <span>Read More</span>
             <svg width="25" height="12" viewBox="0 0 25 12" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -50,7 +22,32 @@
 
 <script>
 export default {
-  name: "Blog"
+  name: "Blog",
+
+  data() {
+    return {
+      blog: [
+        {
+          image: "1",
+          date: "Jan 25, 2019",
+          title: "How one Webflow user grew his single person consultancy from $0-100K in 14 months",
+          description: "See how pivoting to Webflow changed one person’s sales strategy and allowed him to attract"
+        },
+        {
+          image: "2",
+          date: "Jan 25, 2019",
+          title: "How one Webflow user grew his single person consultancy from $0-100K in 14 months",
+          description: "See how pivoting to Webflow changed one person’s sales strategy and allowed him to attract"
+        },
+        {
+          image: "3",
+          date: "Jan 25, 2019",
+          title: "How one Webflow user grew his single person consultancy from $0-100K in 14 months",
+          description: "See how pivoting to Webflow changed one person’s sales strategy and allowed him to attract"
+        },
+      ]
+    }
+  },
 }
 </script>
 
