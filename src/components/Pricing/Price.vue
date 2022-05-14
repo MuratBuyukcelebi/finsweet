47e62f1b1537a6fd19ce0c304fcfea34ca2ce5e3
Murat B.Çelebi <73561693+MuratBuyukcelebi@users.noreply.github.com>
2022-05-14 22:44:07 +0300

    feat: create price componenet

---
+++ src/components/Pricing/Price.vue
@@ -0,0 +1,204 @@
+<template>
+  <div class="pricing-item">
+    <div class="pricing-item__top">
+      <span class="pricing-item__top-price">$299</span>
+      <span class="pricing-item__top-limit">Per Design</span>
+    </div>
+    <div class="pricing-item__location">
+      <div class="pricing-item__location-title">Landing Page </div>
+      <div class="pricing-item__location-description">When you’re ready to go beyond prototyping in Figma, </div>
+    </div>
+    <div class="pricing-item__feature">
+      <div class="pricing-item__feature-item pricing-item__feature-item--active">
+        <div class="pricing-item__feature-item__icon">
+          <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+          </svg>
+        </div>
+        <div class="pricing-item__feature-item__text">All limited links</div>
+      </div>
+      <div class="pricing-item__feature-item pricing-item__feature-item--active">
+        <div class="pricing-item__feature-item__icon">
+          <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+          </svg>
+        </div>
+        <div class="pricing-item__feature-item__text">Own analytics platform</div>
+      </div>
+      <div class="pricing-item__feature-item pricing-item__feature-item--active">
+        <div class="pricing-item__feature-item__icon">
+          <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+          </svg>
+        </div>
+        <div class="pricing-item__feature-item__text">Chat support</div>
+      </div>
+      <div class="pricing-item__feature-item">
+        <div class="pricing-item__feature-item__icon">
+          <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+          </svg>
+        </div>
+        <div class="pricing-item__feature-item__text">Optimize hashtags</div>
+      </div>
+      <div class="pricing-item__feature-item">
+        <div class="pricing-item__feature-item__icon">
+          <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.7767 9.23707C14.5942 9.85191 14.5701 11.0868 13.729 11.6691L3.85392 18.5069C2.85908 19.1958 1.5 18.4838 1.5 17.2737L1.5 3.00968C1.5 1.77272 2.91294 1.06739 3.90155 1.81084L13.7767 9.23707Z" fill="#A8AAB0"/>
+          </svg>
+        </div>
+        <div class="pricing-item__feature-item__text">Unlimited users</div>
+      </div>
+    </div>
+    <a href="#" class="btn btn-secondary pricing-item__button">Get Start</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Price"
+}
+</script>
+
+<style scoped lang="scss">
+.pricing-item {
+  padding: 64px 48px;
+  background: $brand-gray;
+  border-radius: 12px;
+
+  &__top {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    margin-bottom: 20px;
+
+    &-price {
+      font-weight: 600;
+      font-size: 38px;
+      line-height: 56px;
+      color: $brand-dark-blue;
+    }
+    &-limit {
+      font-size: 16px;
+      line-height: 28px;
+      color: $brand-royal-blue;
+    }
+  }
+  &__location {
+    margin-bottom: 50px;
+    min-height: 130px;
+
+    &-title {
+      font-weight: 500;
+      font-size: 24px;
+      line-height: 36px;
+      color: $brand-dark-blue;
+      margin-bottom: 8px;
+    }
+    &-description {
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 28px;
+      opacity: .7;
+      color: $brand-dark-blue;
+    }
+  }
+  &__feature {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    height: 100%;
+    min-height: 268px;
+    margin-bottom: 64px;
+    justify-content: center;
+
+    &-item {
+      display: flex;
+      align-items: center;
+      gap: 12px;
+
+      &__text {
+        font-size: 16px;
+        line-height: 28px;
+        color: $brand-dark-blue;
+        opacity: 0.7;
+      }
+
+      &--active & {
+        &__icon {
+          svg {
+            path {
+              fill: #97DAAD;
+            }
+          }
+        }
+        &__text {
+          opacity: 1;
+        }
+      }
+    }
+  }
+  &__button {
+    width: 100%;
+  }
+
+  &--center {
+    position: relative;
+    background: #1C1E53 !important;
+    z-index: 0;
+    overflow: hidden;
+
+    &::before {
+      content: "";
+      position: absolute;
+      height: 1150px;
+      width: 1150px;
+      background: rgba(255, 255, 255, 0.06);
+      border-radius: 100%;
+      bottom: 155px;
+      left: -84%;
+      z-index: 0;
+      pointer-events: none;
+    }
+  }
+  &--center & {
+    z-index: 1;
+
+    &__top {
+      position: relative;
+      z-index: 1;
+
+      &-price {
+        color: #fff;
+      }
+      &-limit {
+        color: $brand-yellow;
+      }
+    }
+    &__location {
+      position: relative;
+      z-index: 1;
+
+      &-title {
+        color: #fff;
+      }
+      &-description {
+        color: #fff;
+      }
+    }
+    &__feature {
+      position: relative;
+      z-index: 1;
+
+      &-item {
+        &__text {
+          color: #fff;
+        }
+      }
+    }
+    &__button {
+      width: 100%;
+    }
+  }
+}
+</style>
